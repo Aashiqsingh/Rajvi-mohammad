a9630c8834c2cfd238920afde4c7ad7938754696
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-05-10 12:28:56 +0530

    string

---
+++ String/stringDemo1.js
@@ -0,0 +1,43 @@
+
+console.log(str);
+
+// var x = str.length
+// console.log(x);
+
+// str = str.toUpperCase()
+// console.log(str);
+
+// str = str.toLocaleLowerCase()
+// console.log(str)
+
+
+// var x = str.charAt(4)
+// console.log(x);
+
+// var x = str.charCodeAt(40)
+// console.log(x);
+
+// var str = str.codePointAt(40)
+// console.log(str);
+
+// var x = str.concat("The")
+// console.log(x);
+
+// console.log(str + "The");
+
+// let a = 10,b=20;
+// console.log("addition ",a+b);
+
+var str = "RoyAl technosoft pvt ltd."
+
+var str2 = " Rajvi shukla."
+
+// var x = str.concat(str2)
+// var x = str2.concat(str)
+// console.log(x);
+
+
+// var x = str.endsWith("d.")
+
+var x = str2.startsWith(" ")
+console.log(x);
